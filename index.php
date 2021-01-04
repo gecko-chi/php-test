4bc7da9dbdebfe72436dc05647e73b9af8d4727b
root <root@ocmaster01.geckotech.cn>
2021-01-04 22:10:35 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     echo "This is a PHP Test! \n";
-    echo "Serve IP is ".$_SERVER["REMOTE_ADDR"];
+    if(isset($_SERVER['SERVER_ADDR'])){
+        $serverip = $_SERVER['SERVER_ADDR'];
+    }
+    elseif(isset($_SERVER['LOCAL_ADDR'])){
+        $serverip = $_SERVER['LOCAL_ADDR'];
+    }
+    echo "Serve IP is [ ".$serverip." ]\n\n";
    // phpinfo();
 ?>
